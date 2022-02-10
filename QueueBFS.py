cdeffebca26e89d13dbe6d7074ed34ba03568977
chantrex <chantrex@hotmail.com>
2022-02-10 09:47:24 -0600

    Exercise 1 completed

--- QueueBFS.py
+++ QueueBFS.py
@@ -22,27 +22,34 @@ def BFS_christopher(graphX, person1, person2):
     queue.append(root)    
     visited.append(root.state.name)
     # check if there is something in queue to dequeue
+
+    #you can print the content of the queue
     while len(queue) > 0:
         
         #get first item in queue
         currentNode = queue.popleft()
-        
-        #print (("-- dequeue --"), currentNode.state.name)
-        
-        #check if this is goal state
+
+        #check if this is goal state :: return 1
         if currentNode.state.name == person2:
             print ("reached goal state")
-            #print the path
             print ("----------------------")
             print ("This is the Path that tou need to follow for mmeeting too ", person2)
             currentNode.printPath()
-            bestPath = currentNode
-            return False
+            print ("----------------------")
+            print ("Tree")
+            root.printTree()
+            return 1
             break
+        
+        # Try to find if friend exists
         try:
             childStates = currentNode.state.successorFunction()
         except:
+            # Does not exist the first friend :: return 2
             print ("Frien does not exist")
+            print ("----------------------")
+            print ("Tree")
+            root.printTree()            
             return 2
             continue
 
@@ -60,19 +67,25 @@ def BFS_christopher(graphX, person1, person2):
                 #add to tree and queue
                 currentNode.addChild(childNode)
                 queue.append(childNode)                        
-    #print tree
-    print ("----------------------")
+    #Does not exist relation to goal frined :: return 3
     print ("Tree")
-    root.printTree()
-    return bestPath
+    root.printTree()  
+    return 3
 
-print ("Get")
 graphX = "Graph"
-person1 = input("Please, Introduce your origin person : ")
-person2 = input("Please, Introduce your goal person : ")
 option = 0
-option = BFS_christopher(graphX, person1, person2)
-option
+while option != 1:
+    person1 = input("Please, Introduce your origin person : ")
+    person2 = input("Please, Introduce your goal person : ")
+    option = BFS_christopher(graphX, person1, person2)
+    if option == 1:
+            print ("You find the way to meet :", person2)
+    if option == 2:
+            print ("Sorry. we can't find :", person1)
+    if option == 3:
+            print ("Sorry. no one knows ", person1)
+
+print("Thank you..")
 
 
     
